f63cdefff3bcb4478d14248fef2872a1dc0ca69f
amhanpp <amhanpp@gmail.com>
2021-01-22 21:10:56 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -27,7 +27,7 @@ let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭�
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 let helpAuthor = false;
-const randomCount = $.isNode() ? 0 : 7;
+const randomCount = $.isNode() ? 20 : 5;
 const inviteCodes = [
   `UkZvPbI@eU9YaLrjN68g9GvWmnVF1A@eU9Yau7gZ6km9j3cmXRAgg@eU9YaOm7N_hw8G3QnnJG3g@eU9YarrmZfwgpG-GyyJFhw`,
   `UkZvPbI@eU9YaLrjN68g9GvWmnVF1A@eU9Yau7gZ6km9j3cmXRAgg@eU9YaOm7N_hw8G3QnnJG3g@eU9YarrmZfwgpG-GyyJFhw`
